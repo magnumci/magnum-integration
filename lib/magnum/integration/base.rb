8dc917d2ce83daee2426a4ad970dbb0eb50aaff7
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-08-20 19:14:59 -0500

    Get gitlab working

--- lib/magnum/integration/base.rb
+++ lib/magnum/integration/base.rb
@@ -1,19 +1,5 @@
 module Magnum::Integration
   class Base
-    def repositories
-      raise "Implementation required"
-    end
-
-    def repository(id)
-      raise "Implementation required"
-    end
-
-    def deploy_keys(repository_id)
-      raise "Implementation required"
-    end
-
-    def deploy_key(repository_id, id)
-      raise "Implementation required"
-    end
+    # TODO: Finalize structure
   end
 end
--- lib/magnum/integration/gitlab.rb
+++ lib/magnum/integration/gitlab.rb
@@ -1,4 +1,48 @@
+require "gitlab"
+
 module Magnum::Integration
   class Gitlab < Base
+    def initialize(private_token)
+      @private_token = private_token
+    end
+
+    def repositories
+      client.projects.map { |r| format_repository(r) }
+    end
+
+    def repository(id)
+      format_repository(client.project(id))
+    end
+
+    def hooks(repo)
+      client.project_hooks(repo)
+    end
+
+    def create_hook(repo, url)
+      client.add_project_hook(repo, url)
+    end
+
+    def delete_hook(repo, id)
+      client.delete_project_hook(repo, id)
+    end
+
+    private
+
+    def client
+      @client ||= ::Gitlab::Client.new(
+        endpoint: "https://gitlab.com/api/v3",
+        private_token: @private_token
+      )
+    end
+
+    def format_repository(repo)
+      {
+        id:          repo.id,
+        name:        repo.name,
+        description: repo.description,
+        source_url:  repo.ssh_url_to_repo,
+        private:     !repo.public
+      }
+    end
   end
 end
